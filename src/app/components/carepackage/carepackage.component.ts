aa0b363e976c2879849f4e35978b3280afefc411
Scott-Tan <sctan6688@gmail.com>
2019-10-08 09:26:26 -0500

    before merging ignores from master

---
+++ src/app/components/carepackage/carepackage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carepackage',
+  templateUrl: './carepackage.component.html',
+  styleUrls: ['./carepackage.component.css']
+})
+export class CarepackageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

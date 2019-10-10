147ba2958661d4825132cfa5e7c50cb708e0c286
mayuri5amrutkar <55195997+mayuri5amrutkar@users.noreply.github.com>
2019-10-10 09:33:45 -0500

    ma6

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -32,6 +32,12 @@ export class LoginComponent implements OnInit {
   tryLogin(nf: NgForm) {
     this.auth.doLogin(nf.value)
         .then(res => {
+          if(this.auth.userData.role=="Admin"){
+            this.router.navigate(['/admin'])
+          }
+          else if(this.auth.userData.role=="Subscriber"){
+            this.router.navigate(['/subcriber'])
+          }
             console.log(res);
             this.errorMessage = '';
             this.successMessage = 'Logging In!';
